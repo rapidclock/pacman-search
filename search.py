5413792ac987f848b5787121acc064de54c5b2d1
Rahul Thomas <rapidclock@users.noreply.github.com>
2017-02-04 21:32:00 -0800

    v1.2 updated comments

--- search.py
+++ search.py
@@ -7,19 +7,12 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-"""
-In search.py, you will implement generic search algorithms which are called by
-Pacman agents (in searchAgents.py).
-"""
-
 import util
 
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
-    You do not need to change anything in this class, ever.
     """
 
     def getStartState(self):
@@ -133,32 +126,19 @@ def reconstruct_path(parent, goal_state):
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
     from util import Stack
     stk = Stack()
     return master_search_hyper(stk, problem)
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
     from util import Queue
     queue = Queue()
     return master_search_hyper(queue, problem)
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
     from util import PriorityQueueWithFunction
     # the step cost is the actual cumulative cost from parent to state,
     # as calculated by the master_search algorithm.
@@ -175,7 +155,6 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
     from util import PriorityQueueWithFunction
     # total_cost = cumulative cost from parent to current node +
     #              heuristic cost from current node to goal.
